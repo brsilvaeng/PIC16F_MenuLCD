ddfef167ac80686f6402c4f9f7fb1b3ea541f506
Bruno Rodrigues Silva <brsilva.eng@gmail.com>
2018-03-03 16:12:42 -0300

    projeto exemplo controlando 4 leds

--- src/main.c
+++ src/main.c
@@ -1,13 +1,48 @@
-/*
- * File:   main.c
- * Author: bruno
- *
- * Created on 2 de Março de 2018, 16:04
- */
+#include "main.h"
 
+#define LED1        PORTAbits.RA0
+#define LED2        PORTAbits.RA1
+#define LED3        PORTAbits.RA2
+#define LED4        PORTAbits.RA3
 
-#include <xc.h>
+#define LIGA        1U
+#define DESLIGA     0U
+
+void pausa() {
+    unsigned long i;
+    for (i = 0; i < 100000; i++) {
+        CLRWDT();
+    }
+}
+
+void mudaLed(unsigned char led, unsigned char valor) {
+    switch (led) {
+        case 1: LED1 = valor; break;
+        case 2: LED2 = valor; break;
+        case 3: LED3 = valor; break;
+        case 4: LED4 = valor; break;
+    }
+}
 
 void main(void) {
+    ANSEL = 0;
+    TRISA = 0;
+    PORTA = 0xFF;
+    pausa();
+    PORTA = 0;
+    pausa();
+    unsigned char led = 1;
+    unsigned char valor = LIGA;
+    while (1) {
+        CLRWDT();
+        pausa();
+        mudaLed(led, valor);
+        if (led < 4) {
+            led++;
+        } else {
+            led = 1;
+            valor = (valor == LIGA) ? DESLIGA : LIGA;
+        }
+    }
     return;
 }
